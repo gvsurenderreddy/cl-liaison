b2ea62a34f4152f285e8770f9f3f767582c4d186
Clint Moore <clint@ivy.io>
2012-06-26 02:24:10 -0700

    Macherrrr

--- liaison.lisp
+++ liaison.lisp
@@ -53,6 +53,10 @@
   `(hunchentoot:parameter ,name))
 (defmacro re/kill (pattern target)
   `(cl-ppcre:regex-replace-all ,pattern ,target ""))
+(defmacro w/ajax (&rest body)
+  `(cl-who:with-html-output-to-string
+       (*standard-output* nil :prologue nil :indent nil)
+     ,@body))
 (defmacro w/page (title &rest body)
   `(cl-who:with-html-output-to-string
        (*standard-output* nil :prologue nil :indent t)
@@ -84,6 +88,9 @@
                         (htm
                          (:li (:a :href "/" "Home"))
                          (:li (:a :href "/logout" "Logout"))
+                         (:li (:a :data-toggle "modal"
+                                  :href "#pmodal"
+                                  "Preferences"))
                          (:li (:a :href "#"
                                   :onclick (ps ((@ liaison beacon))
                                                false)
@@ -95,15 +102,15 @@
                         (htm
                          (:li (:a :href "/" "Home"))
                          (:li (:a :href "/login" "Login")))))))))
-              (htm
-               (:script :src "/bs/js/bootstrap.js")
-               (:script :src "/js")
-               (:script :type "text/javascript" :src "http://maps.google.com/maps/api/js?sensor=false&key=AIzaSyDsOVRkRfKm3kBVrUaih3xRPYp6dRe8iZ4")))))))
+             (htm
+              (:script :src "/bs/js/bootstrap.js")
+              (:script :src "/js")
+              (:script :type "text/javascript" :src "http://maps.google.com/maps/api/js?sensor=false&key=AIzaSyDsOVRkRfKm3kBVrUaih3xRPYp6dRe8iZ4"))))))
 
 (defun u/uid ()
   (w/session
    (session-value :uid)))
-(defun w/ajax (msg)
+(defun w/json (msg)
   (setf (hunchentoot:content-type*) "application/json")
   msg)
 (defun unique-id ()
@@ -122,20 +129,104 @@
                  (str msg))))))))
 (defun check/email-exists (email)
   (@-q "users" ($ "email" email)))
+
+(defun page/main ()
+  (let ((my-uid (u/uid)))
+    (no-cache)
+    (w/page "Geo"
+          (:script :type "text/javascript"
+                   (str (ps (var goog_markers ([]))
+                            (var goog_map nil))))
+          (:div :class "container"
+                (:div :class "row"
+                      (:div :id "status" "Loading...")
+                      (:div :class "well" :id "canvas"))
+                (:div :id "pmodal" :class "modal hide fade"
+                  (:div :class "modal-header"
+                    (:button :type "button" :class "close" :data-dismiss "modal" "x")
+                    (:h4 "Control Panel"))
+                  (:div :class "modal-body"
+
+                    (:div :class "accordion" :id "cpanel"
+
+                      (:div :class "accordion-group"
+                        (:div :class "accordion-heading"
+                          (:a :class "accordion-toggle"
+                              :data-toggle "collapse"
+                              :data-parent "#cpanel"
+                              :href "#friends" "Friends"))
+                        (:div :id "friends"
+                              :class "accordion-body collapse in"
+                          (:div :class "accordion-inner"
+                            "This is a list of your friends.")))
+
+                      (:div :class "accordion-group"
+                        (:div :class "accordion-heading"
+                          (:a :class "accordion-toggle"
+                              :data-toggle "collapse"
+                              :data-parent "#cpanel"
+                              :href "#filters" "Filters"))
+                        (:div :id "filters"
+                              :class "accordion-body collapse"
+                          (:div :class "accordion-inner"
+                            "This is a list of your filters.")))
+
+
+                      (:div :class "accordion-group"
+                        (:div :class "accordion-heading"
+                          (:a :class "accordion-toggle"
+                              :data-toggle "collapse"
+                              :data-parent "#cpanel"
+                              :href "#prefs" "Preferences"))
+                        (:div :id "prefs"
+                              :class "accordion-body collapse"
+                          (:div :class "accordion-inner"
+                            (:form :class "form-vertical" :method "POST" :action "/preferences"
+                              (:input :type "hidden" :name "uid" :value (str my-uid))
+                              (:h4 "Gender")
+                              (:p (:select :name "gender"
+                               (:option :value "noselect" "Optional, but recommended.")
+                               (:option :value "male" "Female")
+                               (:option :value "female" "Female")
+                               (:option :value "male tg" "Male Transgendered")
+                               (:option :value "female tg" "Female Transgendered")
+                               (:option :value "very male" "Alpha Male")
+                               (:option :value "femmy" "Male, but a little female.")
+                               (:option :value "very female" "Alpha Female")
+                               (:option :value "butchy" "Female, somewhat masculine.")))
+                              (:h4 "Name")
+                              (:p (:input :type "text" :name "name" :placeholder "or pseudonym"))
+                              (:h4 "Age")
+                              (:p (:select :name "age"
+                               (:option :value "18-20" "18-20")
+                               (:option :value "20-25" "20-25")
+                               (:option :value "25-30" "25-30")
+                               (:option :value "30-35" "30-35")
+                               (:option :value "35-40" "35-40")
+                               (:option :value "40-45" "40-45")
+                               (:option :value "45-50" "45-50")
+                               (:option :value "50-55" "50-55")
+                               (:option :value "55-60" "55-60")
+                               (:option :value "60-65" "60-65")
+                               (:option :value "65-70" "65-70")
+                               (:option :value "70+" "70+")))
+                              (:button :class "btn btn-primary" "Save")))))
+
+                      (:div :class "accordion-group"
+                        (:div :class "accordion-heading"
+                          (:a :class "accordion-toggle"
+                              :data-toggle "collapse"
+                              :data-parent "#cpanel"
+                              :href "#devel" "Developer Console"))
+                        (:div :id "devel"
+                              :class "accordion-body collapse"
+                          (:div :class "accordion-inner"
+                            "Developer Controls!"))))))))))
+
 (defun hash-password (pas)
   (ironclad:byte-array-to-hex-string
    (ironclad:digest-sequence :sha256
     (ironclad:ascii-string-to-byte-array pas))))
-(defun page/main ()
-  (no-cache)
-  (w/page "Geo"
-          (:script :type "text/javascript"
-                   (str (ps (var goog_markers ([]))
-                            (var goog_map nil))))
-     (:div :class "container"
-       (:div :class "row"
-         (:div :id "status" "Loading...")
-         (:div :class "span12 well" :id "canvas")))))
 (defun handler/login ()
   (labels ((login-page ()
              (w/page "Login"
@@ -227,60 +318,13 @@
     (if (eq (hunchentoot:request-method*) :POST)
         (register-action)
         (register-page))))
-(defun ajax/beacon ()
-  (w/logged-in
-   (labels ((normalize-name (f)
-              (re/kill "position"
-                 (re/kill "coords"
-                     (re/kill "\\]+|\\[+" f)))))
-     (let* ((new-doc (make-document))
-            (my-uid (w/session (u/uid)))
-            (all-keys (hunchentoot:post-parameters*)))
-       (if (< 0 (length all-keys))
-           (progn
-             (loop for (a . b) in all-keys
-                do (progn
-                     (or (string= b "null")
-                         (add-element (normalize-name a) b new-doc))))
-             (add-element "uid" my-uid new-doc)
-             (add-element "timestamp" (cl-mongo:now) new-doc)
-             (db.save "beacon" new-doc)
-             (w/ajax "{result:'true'}"))
-           (w/ajax "{result:'failed'}"))))))
 (defun handler/site-css ()
   (setf (hunchentoot:content-type*) "text/css")
   (css-lite:css
-    ((:body) (:margin-bottom "0px"))))
-(defun ajax/load-map ()
-  (no-cache)
-  (let* ((cts (chronicity:parse "30 minutes ago"))
-         (cts-sec (chronicity:sec-of cts))
-         (cts-min (chronicity:minute-of cts))
-         (cts-hour (chronicity:hour-of cts))
-         (cts-day (chronicity:day-of cts))
-         (cts-month (chronicity:month-of cts))
-         (cts-year (chronicity:year-of cts))
-         (mongo-timestamp (date-time cts-sec
-                                     cts-min
-                                     cts-hour
-                                     cts-day
-                                     cts-month
-                                     cts-year))
-         (people (docs (iter (db.find "beacon" ($
-                                                ($ "timestamp"
-                                                   ($ "$gte" mongo-timestamp)))
-                                      :limit 0)))))
-    (w/ajax
-     (jsown:to-json
-      (mapcar #'(lambda (person)
-                 (let ((po (empty-object)))
-                   (setf (jsown:val po "latitude")
-                         (get-element "latitude" person))
-                   (setf (jsown:val po "longitude")
-                         (get-element "longitude" person))
-                   (setf (jsown:val po "uid")
-                         (get-element "uid" person))))
-             people)))))
+    ((:body) (:margin-bottom "0px"))
+    ((".prefs") (:margin-top "20px"
+                 :padding-left "20px"
+                 :padding-right "50px"))))
 (defun handler/site-js ()
   (no-cache)
   (setf (hunchentoot:content-type*) "text/javascript")
@@ -289,6 +333,11 @@
      liaison
      (create
 
+      showprefs (lambda ()
+                  ((@ ($ (@ "#prefs")) load) "/preferences")
+                  ((@ ($ (@ "#prefs")) modal) (create backdrop t
+                                                      keyboard t
+                                                      show t)))
       init (lambda ()
              (if navigator.geolocation
                  ((@ navigator geolocation get-Current-Position)
@@ -349,17 +398,68 @@
                        true)
                      (lambda () false))))
       mkimage (lambda ()
-               (new ((@ google maps -Marker-Image) "/girls_marker.gif"
+               (new ((@ google maps -Marker-Image) "/girls_marker.png"
                      (new ((@ google maps -Size) 20 32))
                      (new ((@ google maps -Point) 0 0))
-                     (new ((@ google maps -Point) 0 32)))))
-      mkshadow (lambda ()
-                 (new ((@ google maps -Marker-Image) "images/beachflag_shadow.png"
-                       (new ((@ google maps -Size) 37 32))
-                       (new ((@ google maps -Point) 0 0))
-                       (new ((@ google maps -Point) 0 32)))))))
+                     (new ((@ google maps -Point) 0 32)))))))
+      ;; mkshadow (lambda ()
+      ;;            (new ((@ google maps -Marker-Image) "images/beachflag_shadow.png"
+      ;;                  (new ((@ google maps -Size) 37 32))
+      ;;                  (new ((@ google maps -Point) 0 0))
+      ;;                  (new ((@ google maps -Point) 0 32)))))))
     
     ((@ ($ document) ready) (lambda ()
                               ((@ liaison init))
                               (set-Interval (@ liaison beacon) 30000)))))
 
+(defun ajax/load-map ()
+  (no-cache)
+  (let* ((cts (chronicity:parse "four days ago"))
+         (cts-sec (chronicity:sec-of cts))
+         (cts-min (chronicity:minute-of cts))
+         (cts-hour (chronicity:hour-of cts))
+         (cts-day (chronicity:day-of cts))
+         (cts-month (chronicity:month-of cts))
+         (cts-year (chronicity:year-of cts))
+         (mongo-timestamp (date-time cts-sec
+                                     cts-min
+                                     cts-hour
+                                     cts-day
+                                     cts-month
+                                     cts-year))
+         (people (docs (iter (db.find "beacon"
+                                      ($
+                                       ($ "timestamp"
+                                          ($ "$gte" mongo-timestamp)))
+                                      :limit 20)))))
+    (w/json
+     (jsown:to-json
+      (mapcar #'(lambda (person)
+                 (let ((po (empty-object)))
+                   (setf (jsown:val po "latitude")
+                         (get-element "latitude" person))
+                   (setf (jsown:val po "longitude")
+                         (get-element "longitude" person))
+                   (setf (jsown:val po "uid")
+                         (get-element "uid" person))))
+             people)))))
+(defun ajax/beacon ()
+  (w/logged-in
+   (labels ((normalize-name (f)
+              (re/kill "position"
+                 (re/kill "coords"
+                     (re/kill "\\]+|\\[+" f)))))
+     (let* ((new-doc (make-document))
+            (my-uid (w/session (u/uid)))
+            (all-keys (hunchentoot:post-parameters*)))
+       (if (< 0 (length all-keys))
+           (progn
+             (loop for (a . b) in all-keys
+                do (progn
+                     (or (string= b "null")
+                         (add-element (normalize-name a) b new-doc))))
+             (add-element "uid" my-uid new-doc)
+             (add-element "timestamp" (cl-mongo:now) new-doc)
+             (db.save "beacon" new-doc)
+             (w/json "{result:'true'}"))
+           (w/json "{result:'failed'}"))))))
